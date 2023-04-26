758bd91a48aea902729b9ead5907d7f52cb80ed9
Rubens Parente <rubensparente@yahoo.com.br>
2023-04-26 20:47:39 -0300

    Primeiro Commit

---
+++ src/main/java/me/dio/sacola/SacolaApplication.java
@@ -0,0 +1,13 @@
+package me.dio.sacola;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SacolaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SacolaApplication.class, args);
+	}
+
+}
