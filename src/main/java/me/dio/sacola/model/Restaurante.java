1f224351e94986be9c939413d9c62e923de56a0e
Rubens Parente <rubensparente@yahoo.com.br>
2023-04-26 21:58:32 -0300

    Atualizar Classe Restaurante

--- src/main/java/me/dio/sacola/model/Restaurante.java
+++ src/main/java/me/dio/sacola/model/Restaurante.java
@@ -1,4 +1,26 @@
 package me.dio.sacola.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.util.List;
+
+@AllArgsConstructor
+@Builder
+@Data
+@Entity
+@JsonIgnoreProperties({"hibernateLazyInitializer","handler"})
+@NoArgsConstructor
 public class Restaurante {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String nome;
+    @OneToOne(cascade = CascadeType.ALL)
+    private List<Produto> cardapio;
+    private Endereco endereco;
 }
